cce436cf83f1aba65bbb3e5f4a46e5815ce9282e
Tigran Soghomonyan <soghomonyantigran@gmail.com>
2022-10-19 08:42:58 +0400

    Basic skeleton: Home page, Not found and Users components. Routing. Lazy loading Users component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {
+    path: 'home',
+    component: HomepageComponent,
+    title: 'Homepage',
+  },
+  {
+    path: 'users',
+    loadComponent: () =>
+      import('./users/users.component').then((m) => m.UsersComponent),
+    title: 'Users',
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+    title: '404',
+  },
+];
